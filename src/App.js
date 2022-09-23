2ad1756c6b6b3118f1d800421476119e380e6441
kaynekaynekayne <theygoon@gmail.com>
2022-09-24 01:03:46 +0900

    newww test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect } from "react";
+import axios from "axios";
 function App() {
+
+  const word="word"
+  useEffect(()=>{
+    axios.get("http://localhost:4027/api/todo",{
+      params:{word}
+    })
+    // fetch('http://localhost:4027/api/todo')
+    // .then((response)=>response.json())
+    // .then(data=>console.log(data))
+
+  },[])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO LIST</h1>
     </div>
   );
 }
